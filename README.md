ccd37ae9290e55f89c4744c3eca542ebae33e01b
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-11-13 20:57:17 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# MineiaGo
+
+A proxy to bridge Minecraft PC with Minecraft Pocket Edition and Windows 10 Edition version ```0.15.x```.
+
+## Requirements
+* Node.js 6.1.0 or above
+* Windows, MacOS or Linux (only tested on MacOS)
+
+## Install
+```
+$ git clone https://github.com/filfat/MineiaGo.git && cd MineiaGo && npm install
+```
+
+## Usage
+It's still in early development, a.k.a "not ready for production"!
+```
+$ npm start
+```
+
+## License
+MIT © [Filiph Sandström](https://www.filiphsandstrom.com)
---
+++ server.js
@@ -0,0 +1,64 @@
+// MineiaGo
+// Copyright (C) 2016  Filiph Sandström
+
+'use strict';
+
+/* Set globals */
+var path    = require('path');
+global.sdk  = path.dirname(require.main.filename) + '/lib';
+
+/* Load modules */
+let node            = process,
+    chalk           = require('chalk'),
+    log             = require('./lib/util/log'),
+    server          = require('./lib/controllers/server'),
+    pack            = require('./package.json'),
+    MCPEColor       = require('node-mcpe-color-parser'),
+    command         = require('./lib/controllers/command');
+
+/* Announce */
+log(chalk.bgCyan('MineiaGo') + ' by ' + pack.author, 0);
+log('Starting ' + chalk.bgCyan('MineiaGo') + ' version ' + require(global.sdk + '/util/version')() + '...', 0);
+
+/* Load config */
+global.config = require(global.sdk + '/controllers/config')();
+
+/* Init Server */
+server.init();
+
+/* Handle console input */
+node.stdin.setEncoding('utf8');
+node.stdin.on('data', (cmd) => {
+    //FIXME: metadata
+    cmd = (cmd + '').trim();
+    command.runCommand(cmd, global.server.serverPlayer, (err, res) => {
+        if (err)
+            log(err, 1);
+        else if (res)
+            log(res, 0);
+    });
+});
+
+/* Handle console output */
+function chatHandler (message, sender) {
+    log(MCPEColor(message), 0);
+}
+global.server.chat.on('global.users', chatHandler);
+global.server.chat.on('private.server', chatHandler);
+
+/* Fatal Error */
+node.on('uncaughtException', function (err) {
+    console.log(err);
+    log(err.stack, 3);
+});
+
+/* Handle shutdown */
+function onForceShutdown () {
+    //TODO
+}
+function onShutdown () {
+    node.exit();
+}
+node.on('exit', onForceShutdown.bind());
+node.on('beforeExit', onShutdown.bind());
+node.on('SIGINT', onShutdown.bind());
