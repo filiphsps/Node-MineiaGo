3781014efc0f41f735f99b44e85cfba025b012ea
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-12-09 18:21:48 +0100

    Add config command. Save config.

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ let node            = process,
     command         = require('./lib/controllers/command'),
     readline        = require('readline');
 
+/* Clear console */
+process.stdout.write("\u001b[2J\u001b[0;0H");
+
 /* Handle console.log */
 function fixStdoutFor(cli) {
     var oldStdout = process.stdout;
